6757cbcb62817870935bb88fb67c0e06f343ea1f
Rodrigo Ferreira <83211924+Rodrrf@users.noreply.github.com>
2023-05-03 18:28:16 +0100

    Update CamVigilancia.java

--- TP/src/main/java/com/sample/CamVigilancia.java
+++ TP/src/main/java/com/sample/CamVigilancia.java
@@ -2,13 +2,23 @@ package com.sample;
 
 public class CamVigilancia {
 	Divisao divisao;
-	boolean ligado;
-	
-	public CamVigilancia(Divisao divisao, boolean ligado) {
+	boolean ligado; //  0 - camara desligada 1 - camara ligada
+	public CamVigilancia(Divisao divisao, boolean ligado, boolean detecao) {
 		super();
 		this.divisao = divisao;
 		this.ligado = ligado;
+		this.detecao = detecao;
 	}
+
+	public boolean isDetecao() {
+		return detecao;
+	}
+
+	public void setDetecao(boolean detecao) {
+		this.detecao = detecao;
+	}
+
+	boolean detecao; //  0 - horario de detecao desativado 1 - horario de detecao ativado
 
 	public Divisao getDivisao() {
 		return divisao;
