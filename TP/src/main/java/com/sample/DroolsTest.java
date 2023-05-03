c6ba2c1e34a23c85b5b444f5d48efd9458556d16
Rodrigo Ferreira <83211924+Rodrrf@users.noreply.github.com>
2023-05-03 15:42:51 +0100

    teste

--- TP/src/main/java/com/sample/DroolsTest.java
+++ TP/src/main/java/com/sample/DroolsTest.java
@@ -17,41 +17,13 @@ public class DroolsTest {
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
 
             // go !
-            Message message = new Message();
-            message.setMessage("Hello World");
-            message.setStatus(Message.HELLO);
-            kSession.insert(message);
+            
+            //kSession.insert(message);
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
 
-    public static class Message {
-
-        public static final int HELLO = 0;
-        public static final int GOODBYE = 1;
-
-        private String message;
-
-        private int status;
-
-        public String getMessage() {
-            return this.message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-    }
 
 }
