2d20d79ef53f865c4b5afc37e34cd6364becebad
Pontes <p.antunespontes@gmail.com>
2023-05-03 16:16:58 +0100

    update classes

--- TP/src/main/java/com/sample/DroolsTest.java
+++ TP/src/main/java/com/sample/DroolsTest.java
@@ -18,7 +18,25 @@ public class DroolsTest {
 
             // go !
             
-            //kSession.insert(message);
+        	Divisao d1 = new Divisao ("rua");
+       	 	Divisao d2 = new Divisao ("escritório");
+       	 	Divisao d3 = new Divisao ("sala");
+       	 	kSession.insert( d1 );
+       	 	kSession.insert( d2 );
+       	 	kSession.insert( d3 ); 
+       	 	//rua n tem sensor de movimento
+       	 	SensorMov asp2 = new SensorMov( d2, false ); //escritório tem aspersor
+       	 	SensorMov asp3 = new SensorMov( d3, false);
+       	 	kSession.insert( asp1 ); 
+       	 	kSession.insert( asp2 );
+       	 	kSession.insert( asp3 );
+       	 
+       	 	Fogo f1 = new Fogo (d1); //Fogo na cozinha
+       	 	Fogo f2 = new Fogo (d2); //Fogo no escritório
+       	 	Fogo f3 = new Fogo (d3); //Fogo na sala
+       	 	kSession.insert( f1 );
+       	 	kSession.insert( f2 );
+       	 	kSession.insert( f3 ); 
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
