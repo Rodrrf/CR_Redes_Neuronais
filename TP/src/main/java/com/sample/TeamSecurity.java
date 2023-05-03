09343f2eb781175a475c3c51eba17d460a079081
Rodrigo Ferreira <83211924+Rodrrf@users.noreply.github.com>
2023-05-03 16:59:54 +0100

    classe TeamSecurity

---
+++ TP/src/main/java/com/sample/TeamSecurity.java
@@ -0,0 +1,23 @@
+package com.sample;
+
+public class TeamSecurity {
+		private String nomeTeam;
+		private int numero;
+		
+		public TeamSecurity(String nomeTeam, int numero) {
+			super();
+			this.nomeTeam = nomeTeam;
+			this.numero = numero;
+		}
+
+		public String getNomeTeam() {
+			return nomeTeam;
+		}
+
+		public int getNumero() {
+			return numero;
+		}
+		
+		
+		
+}
